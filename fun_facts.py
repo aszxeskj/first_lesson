2efa7138a0311144e036fa043db589aa6d75d07e
aszxeskj <98270597+aszxeskj@users.noreply.github.com>
2022-01-30 18:56:21 +0300

    Add files via upload

---
+++ fun_facts.py
@@ -0,0 +1,21 @@
+
+# name = input('привет напиши свое имя: ')
+# age = int(input('введите количествво прожитых лет: '))
+# weight = int(input('введите количество килограммов в твоей оболочке(вес): '))
+#
+# sec = age * 365 * 24 * 60  * 60
+# print(f'\n твой возраст в секундах: {sec} ')
+#
+# moon_weight =  weight //6
+# print(f'\n  твой вес на луне: {moon_weight}')
+# input('введите ентер')
+string = 'сидел барсук в своей норе и ел картошечку пюре'
+a = '!'
+c = 'ре'
+print(len(string))
+print(string+a)
+
+if_c in string:
+    print(f'{c} есть!')
+else:
+    print(f'{c} нет!')
--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-a = 5
-b = 25
-c = a ** b
-print(c)
+# print ('опрос')
+
+# fname  ='egor'
+# lname = 'zuravliov'
+
+# print (fname)
+# print (lnam
+# input('нажмите Enter')
+
+# fname = str
+# print(fname)
+
+# a = input('ведите свое имя:')
+
+# b =int(input('ведите ваш возраст:'))
+
+# print(type(a))
+# print(type(b))
+# a = 10
+# c = str(a)
+# b = 2
+# print(c + b)
+# print("""тут что-то не так, \n не будь я д'Артаньян"-подумал он.""")
+
+string = "всем привет я люблю python"
+
+print(f'исходная фраза: {string}')
+
+print (f'\n когда я кричу: {string.upper()}')
+
+print (f'\n когда я говорю шопотом: {string. lower()}')
+
+print (f'\n с заменой: {string.replace("любл ","обожаю")}')
+
+input('введите ENTER для завершения )
