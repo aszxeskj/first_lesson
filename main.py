635aac56a323dec527522aff950c16362829e453
aszxeskj <98270597+aszxeskj@users.noreply.github.com>
2022-02-13 17:25:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-otvet = ""
-while otvet.lower() != "покупаю":
-    otvet = input ("купи слона!\n")
-    if otvet.lower() != "покупаю":
-        print (f"все говорят: {otvet},  ты возьми и купи слона! ")
-print("\nцена слона 1000$. деньги вперед")
-
+num =0
+while num <9:
+    num=num + 1
+    print(num)
